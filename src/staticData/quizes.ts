9825bc91ad5df2e11602d2541040ea47df95c2f2
vyamashiro <vkatsuo@gmail.com>
2022-12-26 20:12:14 +0000

    add more questions

--- src/staticData/quizes.ts
+++ src/staticData/quizes.ts
@@ -62,4 +62,144 @@ export const quizes = [
       },
     ]
   },
+  {
+    "id_question": "00003",
+    "question": "O que faz a tag <hr> ?",
+    "alternatives": [
+      {
+        "id_alternative": "01",
+        "alternative": "É a tag responsável por criar o cabeçalho da tabela",
+        "isCorrect": false,
+      },
+      {
+        "id_alternative": "02",
+        "alternative": "Cria uma linha horizontal que não pode ser estilizada usando CSS",
+        "isCorrect": false,
+      },
+      {
+        "id_alternative": "03",
+        "alternative": "Cria uma linha horizontal que pode ser estilizada usando CSS",
+        "isCorrect": true,
+      },
+      {
+        "id_alternative": "04",
+        "alternative": "É a tag responsável pela criação de header",
+        "isCorrect": false,
+      },
+      {
+        "id_alternative": "05",
+        "alternative": "Nenhuma das alternativas",
+        "isCorrect": false,
+      },
+    ]
+  },
+  {
+    "id_question": "00004",
+    "question": "Das opções abaixo quais tags não possuem fechamento ?",
+    "alternatives": [
+      {
+        "id_alternative": "01",
+        "alternative": "div, span e input",
+        "isCorrect": false,
+      },
+      {
+        "id_alternative": "02",
+        "alternative": "br, hr, img e  input",
+        "isCorrect": true,
+      },
+      {
+        "id_alternative": "03",
+        "alternative": "p, br e table",
+        "isCorrect": false,
+      },
+      {
+        "id_alternative": "04",
+        "alternative": "Todas as opções anteriores",
+        "isCorrect": false,
+      },
+      {
+        "id_alternative": "05",
+        "alternative": "Nenhuma das opções anteriores",
+        "isCorrect": false,
+      },
+    ]
+  },
+  {
+    "id_question": "00005",
+    "question": "Para o que serve a tag <br> ?",
+    "alternatives": [
+      {
+        "id_alternative": "01",
+        "alternative": "Informar que o idioma da página é português (brasileiro)",
+        "isCorrect": false,
+      },
+      {
+        "id_alternative": "02",
+        "alternative": "criar uma separação/divisão na página",
+        "isCorrect": false,
+      },
+      {
+        "id_alternative": "03",
+        "alternative": "Quebra de linha",
+        "isCorrect": true,
+      },
+      {
+        "id_alternative": "04",
+        "alternative": "negrito",
+        "isCorrect": false,
+      },
+      {
+        "id_alternative": "05",
+        "alternative": "itálico",
+        "isCorrect": false,
+      },
+    ]
+  },
+  {
+    "id_question": "00006",
+    "question": "A tag responsável por criar hyperlink é a tag <link>. Verdadeiro ou falso ?",
+    "alternatives": [
+      {
+        "id_alternative": "01",
+        "alternative": "Verdadeiro",
+        "isCorrect": false,
+      },
+      {
+        "id_alternative": "02",
+        "alternative": "Falso",
+        "isCorrect": true,
+      },
+    ]
+  },
+  // {
+  //   "id_question": "0000X",
+  //   "question": "",
+  //   "alternatives": [
+  //     {
+  //       "id_alternative": "01",
+  //       "alternative": "",
+  //       "isCorrect": false,
+  //     },
+  //     {
+  //       "id_alternative": "02",
+  //       "alternative": "",
+  //       "isCorrect": true,
+  //     },
+  //     {
+  //       "id_alternative": "03",
+  //       "alternative": "",
+  //       "isCorrect": true,
+  //     },
+  //     {
+  //       "id_alternative": "04",
+  //       "alternative": "",
+  //       "isCorrect": false,
+  //     },
+  //     {
+  //       "id_alternative": "05",
+  //       "alternative": "",
+  //       "isCorrect": false,
+  //     },
+  //   ]
+  // },
 ]
